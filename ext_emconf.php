88d149ce453a7691a88e5d670ae0634921621d0a
NITSAN Technologies <sanjay@nitsan.in>
2021-09-09 15:28:25 +0530

    [TODO] Author name change

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,13 +14,13 @@ $EM_CONF[$_EXTKEY] = [
   'title' => '[NITSAN] YouTube Plugin',
   'description' => 'One of the only TYPO3 extension which provides to integrate all the features of Youtube.com like Simple Video, Channel, Playlist and Live-stream to your TYPO3 site. Live-Demo: https://demo.t3terminal.com/t3t-extensions/youtube/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-youtube-plugin-free/',
   'category' => 'plugin',
-  'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
+  'author' => 'NITSAN Technologies Pvt Ltd',
   'author_email' => 'sanjay@nitsan.in',
   'author_company' => 'NITSAN Technologies Pvt Ltd',
   'state' => 'stable',
   'uploadfolder' => true,
   'clearCacheOnLoad' => 0,
-  'version' => '1.4.3',
+  'version' => '1.4.4',
   'constraints' =>
   [
     'depends' =>
